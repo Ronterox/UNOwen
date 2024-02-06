6c89a439f2b12db3d46aca23b442a49e9b838c10
Ricardo <ricarditomontserrat@gmail.com>
2024-02-06 04:20:36 +0100

    Added backoff for retry of network

--- src/watch.ts
+++ src/watch.ts
@@ -1,14 +1,25 @@
+let timeout: number = 0;
+const BACKOFF = 500;
+
 function watch() {
     const ws = new WebSocket("ws://localhost:8080/ws");
-    ws.onopen = () => { ws.send("Ricardo"); };
+
+    ws.onopen = () => { 
+        ws.send("Ricardo"); 
+        timeout = 0;
+    };
+
     ws.onmessage = (event) => {
         console.log(`Message from server: ${event.data}`);
         if (event.data === "update") location.reload();
     };
+
     ws.onclose = () => { 
         console.log("WebSocket closed"); 
-        setTimeout(() => { watch(); }, 1000);
+        setTimeout(() => { watch(); }, 1000 + timeout * BACKOFF);
+        timeout++;
     };
+
     ws.onerror = (event) => { console.error("WebSocket error observed:", event); }
 }
 
